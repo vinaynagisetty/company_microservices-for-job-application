adf3d267beb1fa0dd53c3c911952244776541311
vinay <vinaynagisetty7@gmail.com>
2025-01-26 22:00:34 +0530

    craeted the company  microservices as part of  job application

---
+++ src/main/java/com/vinay/nagisdetty/company_microservice/CompanyMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vinay.nagisdetty.company_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vinay/nagisdetty/company_microservice/company/CompanyService.java
@@ -0,0 +1,11 @@
+package com.vinay.nagisdetty.company_microservice.company;
+
+import java.util.List;
+
+public interface CompanyService {
+    List<Company> getAllCompanies();
+    boolean updateCompany(Company company, Long id);
+    void createCompany(Company company);
+    boolean deleteCompanyById(Long id);
+    Company getCompanyById(Long id);
+}
